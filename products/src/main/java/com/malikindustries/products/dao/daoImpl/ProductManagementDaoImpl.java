48b287aac7356a10c4be5338fd6870c0595bbe39
Mobile Programming <amritmsps@gmail.com>
2023-07-06 17:19:26 +0530

    Added logic to update product quantity

--- products/src/main/java/com/malikindustries/products/dao/daoImpl/ProductManagementDaoImpl.java
+++ products/src/main/java/com/malikindustries/products/dao/daoImpl/ProductManagementDaoImpl.java
@@ -3,6 +3,7 @@ package com.malikindustries.products.dao.daoImpl;
 import com.malikindustries.products.dao.ProductManagementDao;
 import com.malikindustries.products.model.AddProduct;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -15,9 +16,28 @@ public class ProductManagementDaoImpl implements ProductManagementDao {
     @Override
     public void insertProduct(AddProduct addProduct){
 
-        jdbcTemplate.update("INSERT into ProductTable VALUES (?,?,?,?)",
-                addProduct.getProductId(),addProduct.getStoreId(),
-                addProduct.getProductCategory(),addProduct.getQuantity());
+        Integer id = getId(addProduct);
 
+        if(id==0) {
+            jdbcTemplate.update("INSERT into ProductTable VALUES (?,?,?,?)",
+                    addProduct.getProductId(), addProduct.getStoreId(),
+                    addProduct.getProductCategory(), addProduct.getQuantity());
+        }
+        else{
+            jdbcTemplate.update("UPDATE ProductTable SET QUANTITY = QUANTITY + ? WHERE ID=?",
+                    addProduct.getQuantity(), id);
+
+        }
+
+    }
+
+    private Integer getId(AddProduct addProduct){
+        try {
+            return jdbcTemplate.queryForObject("SELECT ID FROM ProductTable WHERE (STORE_ID=? AND PRODUCT_ID=?) ", Integer.class,
+                    addProduct.getStoreId(), addProduct.getProductId());
+        }
+        catch(EmptyResultDataAccessException e){
+            return 0;
+        }
     }
 }
